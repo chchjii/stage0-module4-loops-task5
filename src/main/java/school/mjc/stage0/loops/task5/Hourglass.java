f3d0a0fc13814e33a84d96dd660c3d40fd3bf1b7
Kirill <cryt.mem@gmail.com>
2025-08-20 18:05:19 +0300

    sol

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -3,20 +3,14 @@ package school.mjc.stage0.loops.task5;
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
         for (int i = 0; i < height; i++) {
-
-            int spaces, symbols;
-
-            if (i <= height / 2) {
-                spaces = i;
-                symbols = height - 2 * i;
-            } else {
-                spaces = height - 1 - i;
-                symbols = height - 2 * spaces;
+            for (int j = 0; j < height; j++) {
+                int spaces = Math.min(i, height-1-i);
+                if (j >= spaces && j < height - spaces) {
+                    System.out.print('8');
+                } else {
+                    System.out.print(' ');
+                }
             }
-            for (int j = 0; j < spaces; j++) System.out.println(" ");
-            for (int k = 0; k < symbols; k++) System.out.println("8");
-
-
             System.out.println();
         }
     }
