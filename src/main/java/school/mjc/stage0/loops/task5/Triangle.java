dbb935e45193bacad6d7019cb959c874bc047bfa
Kirill <cryt.mem@gmail.com>
2025-08-20 18:21:47 +0300

    sol

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,9 +2,14 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-        for (int i = 0; i <= cathetusLength; i++) {
+        for (int i = 1; i <= cathetusLength; i++) {
+
             for (int j = 0; j < i; j++) {
                 System.out.print(8);
+            }
+
+            for (int j = i; j < cathetusLength; j++) {
+                System.out.print(" ");
             }
             System.out.println();
         }
